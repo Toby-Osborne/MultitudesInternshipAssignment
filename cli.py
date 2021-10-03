3957239a74a894e0c547dda0d65d1215820f0ca8
Toby-Osborne <tosb789@aucklanduni.ac.nz>
2021-10-03 22:36:17 +1300

    implemented the command line input for the machine

--- cli.py
+++ cli.py
@@ -1,16 +1,19 @@
-from pprint import pprint
 from github import Github
+import argparse as ap
 
-username = 'microsoft'
-repository = "TypeScript"
-
+print ("Welcome to the Multitudes CLI!  Time to dig some data... ")
+print ("Who's the repo-wner'?")
+username = input()
+print ("Whats the repo's name?")
+repository = input()
+print("Digging for Data...")
 g = Github()
 
 #very nice
 user = g.get_user(username)
 repo = user.get_repo(repository)
-pulls = repo.get_pulls(state='all')
-print(pulls.totalCount)
-
+pulls = repo.get_pulls(state='open')
+print("Found a total of "+ str(pulls.totalCount) +" open pull requests at "+ username+"/"+repository+".")
+print("See you next time :)")
 
 #print(g)
